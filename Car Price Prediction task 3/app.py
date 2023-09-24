dd5b47571235d8062c53ee5891ad45ff79a7c992
Suman mondal <smondal.qwerty@gmail.com>
2023-09-24 23:32:31 +0530

    fix

--- Car Price Prediction task 3/app.py
+++ Car Price Prediction task 3/app.py
@@ -1,18 +1,24 @@
+import warnings
+warnings.filterwarnings("ignore")
 from flask import Flask,render_template,request
 from flask_material import Material
 import pandas as pd
 import pickle
 from sklearn.preprocessing import StandardScaler
+from collections import defaultdict
+
+param = defaultdict(None)
 
 features=['wheelbase','carlength','carwidth','curbweight','enginesize','boreratio','horsepower',
 		'citympg','highwaympg','fueltype_diesel','fueltype_gas','aspiration_std','aspiration_turbo',
 		'doornumber_four','doornumber_two','carbody_convertible','carbody_hardtop','carbody_hatchback',
-		'carbody_sedan','carbody_wagon','drivewheel_4wd','drivewheel_fwd','drivewheel_rwd','enginetype_dohc',
-		'enginetype_dohcv','enginetype_l','enginetype_ohc','enginetype_ohcf','enginetype_ohcv','enginetype_rotor',
-		'cylindernumber_eight','cylindernumber_five','cylindernumber_four','cylindernumber_six',
-		'cylindernumber_three','cylindernumber_twelve','cylindernumber_two','fuelsystem_1bbl','fuelsystem_2bbl',
-		'fuelsystem_4bbl','fuelsystem_idi','fuelsystem_mfi','fuelsystem_mpfi','fuelsystem_spdi','fuelsystem_spfi',
-		'CarsRange_Budget','CarsRange_Medium','CarsRange_Highend']
+		'carbody_sedan','carbody_wagon','drivewheel_4wd','drivewheel_fwd','drivewheel_rwd',
+		'enginetype_dohc','enginetype_dohcv','enginetype_l','enginetype_ohc','enginetype_ohcf',
+		'enginetype_ohcv','enginetype_rotor','cylindernumber_eight','cylindernumber_five',
+		'cylindernumber_four','cylindernumber_six','cylindernumber_three','cylindernumber_twelve',
+		'cylindernumber_two','fuelsystem_1bbl','fuelsystem_2bbl','fuelsystem_4bbl','fuelsystem_idi',
+		'fuelsystem_mfi','fuelsystem_mpfi','fuelsystem_spdi','fuelsystem_spfi','CarsRange_Budget',
+		'CarsRange_Medium','CarsRange_Highend']
 
 nominal=['fueltype','aspiration','doornumber','carbody','drivewheel',
 		'enginetype','cylindernumber','fuelsystem','CarsRange']
@@ -32,7 +38,8 @@ Material(app)
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template("index.html",
+		param = param)
 
 @app.route('/',methods=["POST"])
 def analyze():
@@ -59,21 +66,27 @@ def analyze():
 		if model_choice == 'lreg':
 			model = pickle.load(open('LinearRegression.pkl', 'rb'))
 			prediction = model.predict(new_df)
+
 		elif model_choice == 'dtree':
 			model = pickle.load(open('DecisionTree.pkl', 'rb'))
 			prediction = model.predict(new_df)
+
 		elif model_choice == 'rforest':
 			model = pickle.load(open('RandomForest.pkl', 'rb'))
 			prediction = model.predict(new_df)
+
 		elif model_choice == 'XGB':
 			model = pickle.load(open('XGBoost.pkl', 'rb'))
 			prediction = model.predict(new_df)
+
 		elif model_choice == 'LGBM':
 			model = pickle.load(open('LGBM.pkl', 'rb'))
 			prediction = model.predict(new_df)
+
 		elif model_choice == 'CB':
 			model = pickle.load(open('CatBoost.pkl', 'rb'))
 			prediction = model.predict(new_df)
+
 		elif model_choice == 'avg':
 			average = 0.0
 			for m in models:
